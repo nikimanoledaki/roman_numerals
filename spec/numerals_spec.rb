5d5f0bb0aff8d64450a76a9aca5a9d0bb296a705
niki-01 <niki.manoledaki@gmail.com>
2019-12-19 11:13:40 +0000

    Converts number into roman numeral

---
+++ spec/numerals_spec.rb
@@ -0,0 +1,11 @@
+require 'numerals'
+
+describe RomanNumeral do
+    it "converts number into numeral" do
+      expect(subject.convert(2)).to eq("II")
+    end
+
+    it "raises error if input is not an integer" do
+      expect{ subject.convert("II") }.to raise_error "Not an integer"
+    end
+end
